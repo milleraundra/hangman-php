65f1ca73dd9a5a88e990e0e38681b538d5e707ea
Aundra Miller and torrence stratton <tlstratton@gmail.com>
2016-02-11 16:23:23 -0800

    add initial files and begin creating Hangman class

---
+++ app/app.php
@@ -0,0 +1,23 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Hangman.php";
+
+    // session_start();
+    // if(empty($_SESSION['list_of_albums'])) {
+    //     $_SESSION['list_of_albums'] = array();
+    // }
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path'=> __DIR__.'/../views'));
+
+    $app->get('/', function() use ($app) {
+        $hangman_word = randomWord();
+        $hangman_game = new Hangman($hangman_word);
+        return $app['twig']->render('index.html.twig', array('game'=> $hangman_game));
+    });
+
+    // $app->post('/')
+
+    return $app;
+ ?>
---
+++ src/Hangman.php
@@ -0,0 +1,46 @@
+<?php
+    class Hangman {
+        private $word;
+
+        function __construct($word) {
+            $this->word = $word;
+        }
+
+
+
+        function getWord() {
+            return $this->word;
+        }
+
+
+        // function checkletteragainst word {
+        //
+        //
+        // }
+        //
+        // function insertletterinword {
+        //
+        // }
+        //
+        // function addbodypart/output aka wrong {
+        //     if (6 guesses = true) {
+        //         game over
+        //     }
+        // }
+
+
+    }
+
+    function randomWord() {
+        $wordList = array("guitar", "drums", "timpani", "french horn", "oboe", "bassoon", "cello", "triangle", "clarinet", "flute", "trumpet", "tuba");
+        shuffle($wordList);
+        $chosen_word = $wordList[0];
+        return $chosen_word;
+    }
+
+// "      O     "
+// "     -|-     "
+// "      |     "
+// "     /\     "
+// "    /  \    "
+ ?>
